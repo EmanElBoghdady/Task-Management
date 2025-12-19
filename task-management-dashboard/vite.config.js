0b0932d7597c888846b44d9edb5d94a8501ad04a
EmanElBoghdady <emaelboghdady807060504030@gmail.com>
2025-12-19 10:29:06 +0200

    add vercel config

--- task-management-dashboard/vite.config.js
+++ task-management-dashboard/vite.config.js
@@ -1,33 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/task-management-dashboard/', // Change this to your repository name
-  server: {
-    port: 3000,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          bootstrap: ['bootstrap', 'bootstrap-icons'],
-        }
-      }
-    }
-  }
 })
+
